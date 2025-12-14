533e7af13962ca34428d7eb4505f10d74483c85a
maskatcc <masaaki.katou@gmail.com>
2025-12-14 13:07:25 +0900

    task for deploy funcs

--- src/tasks/deploy.ts
+++ src/tasks/deploy.ts
@@ -1,1 +1,91 @@
-console.info('deploy')
+import fs from 'node:fs'
+import 'dotenv/config'
+import { ENV } from './_env.js'
+import { deployFunc } from './utils/lambda-codeupdate.js'
+import { getFunc } from './utils/lambda-get.js'
+
+// console.info('deploy')
+
+const funcArgIndex = 2
+
+if (process.argv.length < funcArgIndex + 1) {
+  console.error('func arg not found.')
+  process.exit(1)
+}
+
+const func = process.argv[funcArgIndex] || ''
+const workload = process.env.WORKLOAD || 'poc'
+
+if (!fs.existsSync(`dist/deploys/${workload}/${func}.codesha256`)) {
+  console.error('lambda code sha256 not found. maybe func not created')
+  process.exit(1)
+}
+
+const bucketName = ENV.s3Bucket
+const objectKey = `${ENV.lambdaVersion}/${func}.zip`
+
+const deployResult = await deployFunc(func, bucketName, objectKey)
+
+// AWS Lambda関数の状態の追跡 | Amazon Web Services ブログ
+// https://aws.amazon.com/jp/blogs/news/tracking-the-state-of-lambda-functions/
+const deployState = deployResult.State
+const deployUpdateStatus = deployResult.LastUpdateStatus
+const deployMessage = `${deployResult.LastUpdateStatusReason} ${deployUpdateStatus}(${deployResult.LastUpdateStatusReasonCode})...`
+
+if (deployState !== 'Active' || (deployUpdateStatus !== 'Successful' && deployUpdateStatus !== 'InProgress')) {
+  throw new Error(deployMessage)
+}
+
+let codeSha256: string | undefined
+let messageCache = deployMessage
+const waitInterval = 200
+
+process.stdout.write(deployMessage)
+
+const wait = (msec: number): Promise<void> => {
+  return new Promise(resolve => setTimeout(resolve, msec))
+}
+
+await wait(waitInterval)
+
+let getResult = await getFunc(func)
+
+while (true) {
+  const state = getResult.Configuration?.State
+  const updateStatus = getResult.Configuration?.LastUpdateStatus
+
+  if (state === 'Active' && updateStatus === 'Successful') {
+    codeSha256 = getResult.Configuration?.CodeSha256
+    console.info(``)
+    console.info(`lambda deploy successful!`)
+    break
+  }
+
+  if (state !== 'Active' || updateStatus !== 'InProgress') {
+    console.info(``)
+    console.error(`unknown deploy error. ${getResult}`)
+    break
+  }
+
+  const reasonCode = getResult.Configuration?.LastUpdateStatusReasonCode
+  const reason = getResult.Configuration?.LastUpdateStatusReason
+  const message = `${reason} ${updateStatus}(${reasonCode})...`
+
+  if (message === messageCache) {
+    process.stdout.write('.')
+    await wait(waitInterval)
+  }
+  else {
+    console.info(``)
+    process.stdout.write(message)
+    messageCache = message
+  }
+
+  getResult = await getFunc(func)
+}
+
+if (codeSha256) {
+  fs.mkdirSync(`dist/deploys/${workload}`, { recursive: true })
+  fs.writeFileSync(`dist/deploys/${workload}/${func}.codesha256`, codeSha256)
+  console.info(`lambda code sha256 saved.`)
+}
---
+++ src/tasks/sync-from-code.ts
@@ -0,0 +1,53 @@
+import fs from 'node:fs'
+import 'dotenv/config'
+import { getFunc } from './utils/lambda-get.js'
+import { ResourceNotFoundException } from '@aws-sdk/client-lambda'
+
+// console.info('sync from code')
+
+const funcArgIndex = 2
+
+if (process.argv.length < funcArgIndex + 1) {
+  console.error('func arg not found.')
+  process.exit(1)
+}
+
+const func = process.argv[funcArgIndex] || ''
+const workload = process.env.WORKLOAD || 'poc'
+const hashFile = `dist/deploys/${workload}/${func}.codesha256`
+
+let codeSha256: string | undefined = undefined
+
+try {
+  const getResult = await getFunc(func)
+  codeSha256 = getResult.Configuration?.CodeSha256
+}
+catch (error: any) {
+  if (error instanceof ResourceNotFoundException) {
+    console.info(error.message)
+  }
+  else {
+    throw error
+  }
+}
+
+if (codeSha256) {
+  // func deployed (maybe created by others)
+  if (!fs.existsSync(hashFile)) {
+    fs.mkdirSync(`dist/deploys/${workload}`, { recursive: true })
+    fs.writeFileSync(hashFile, codeSha256)
+    console.info(`[lambda sync] create codesha256 in local deploys: ${hashFile}`)
+  }
+  // func code changed (maybe updated by others)
+  else if (fs.readFileSync(hashFile, 'utf-8') !== codeSha256) {
+    fs.writeFileSync(hashFile, codeSha256)
+    console.info(`[lambda sync] update codesha256 in local deploys: ${hashFile}`)
+  }
+}
+else {
+  // func removed (maybe deleted by others)
+  if (fs.existsSync(hashFile)) {
+    fs.rmSync(hashFile, { force: true })
+    console.info(`[lambda sync] delete codesha256 in local deploys: ${hashFile}`)
+  }
+}
